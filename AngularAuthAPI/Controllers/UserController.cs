dac7b994b42a2284d7b9652cfb437184b1f98df8
Gagan B <gaganbacademics6@gmail.com>
2025-07-03 12:23:41 +0530

    Password Hashing done

--- AngularAuthAPI/Controllers/UserController.cs
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using AngularAuthAPI.Helpers;
 
 namespace AngularAuthAPI.Controllers
 {
@@ -23,9 +25,18 @@ namespace AngularAuthAPI.Controllers
             if (userObj == null)
                 return BadRequest(new { Message = "Enter correct details" });
 
-            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username==userObj.Username && x.Password==userObj.Password);
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username==userObj.Username);
+
             if(user == null)
                    return NotFound(new {Message = "User not found!"});
+
+            var storedHashPassword = await _authContext.Users
+                .Where(x => x.Username == userObj.Username)
+                .Select(x => x.Password)
+                .FirstOrDefaultAsync();
+
+            if(PasswordHasher.VerifyPassword(userObj.Password, storedHashPassword) == false)
+                return Unauthorized(new { Message = "Invalid password!" });
             return Ok(new { Message = "Login succesfull!" });
         }
 
@@ -35,10 +46,30 @@ namespace AngularAuthAPI.Controllers
             if (userObj == null)
                 return BadRequest(new { Message = "Enter correct details" });
 
+            if(await CheckUserNameExists(userObj.Username))
+            {
+                return BadRequest(new { Message = "Username already exists!" });
+            }
+            if(await CheckEmailExists(userObj.Email))
+            {
+                return BadRequest(new { Message = "Email already exists!" });
+            }
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User"; // Default role, can be changed later
+            userObj.Token = "";
             await _authContext.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
             return Ok(new { Message = "User registered successfully!" });
         }
+
+        private async Task<bool> CheckUserNameExists(string username) =>
+            await _authContext.Users.AnyAsync(x => x.Username == username);
+
+
+        private async Task<bool> CheckEmailExists(string email) => 
+            await _authContext.Users.AnyAsync(x => x.Email == email);
+
 
     }
 }
---
+++ AngularAuthAPI/Helpers/PasswordHasher.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace AngularAuthAPI.Helpers
+{
+    public class PasswordHasher
+    {
+        private static int SaltSize = 16; // Size of the salt in bytes
+        private static int HashSize = 32; // Size of the hash in bytes
+        private static int Iterations = 100000; // Number of iterations for the PBKDF2 algorithm
+
+        private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512; // Hash algorithm to use
+
+        public static string HashPassword(string password)
+        {
+           byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+                password: password,
+                salt: salt,
+                iterations: Iterations,
+                hashAlgorithm: Algorithm,
+                outputLength: HashSize);
+
+            return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            // Split the hashed password into hash and salt
+            var parts = hashedPassword.Split('-');
+            if (parts.Length != 2)
+            {
+                throw new ArgumentException("Invalid hashed password format.");
+            }
+            byte[] hash = Convert.FromHexString(parts[0]);
+            byte[] salt = Convert.FromHexString(parts[1]);
+            // Hash the provided password with the same salt
+            byte[] computedHash = Rfc2898DeriveBytes.Pbkdf2(
+                password: password,
+                salt: salt,
+                iterations: Iterations,
+                hashAlgorithm: Algorithm,
+                outputLength: HashSize);
+            // Compare the computed hash with the stored hash, we are using this method bcz it avoid malicious attacks
+            return CryptographicOperations.FixedTimeEquals(computedHash, hash);
+        }
+    }
+}
